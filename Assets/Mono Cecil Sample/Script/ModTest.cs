424263e3dd882341a7b5da69438553a240215a31
BelieberNeo <aiczk.398@gmail.com>
2019-09-06 14:41:22 +0900

    add extension

--- Assets/Mono Cecil Sample/Script/ModTest.cs
+++ Assets/Mono Cecil Sample/Script/ModTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Mono.Cecil;
@@ -23,12 +24,12 @@ namespace Mono_Cecil_Sample.Script
             EditorApplication.LockReloadAssemblies();
             try
             {
-                var mainAssembly = CecilUtility.GetAssembly("Assembly-CSharp");
+                var mainAssembly = CecilUtility.GetAssembly("Assembly-CSharp").ToAssemblyDefinition();
                 var editorAssembly = CecilUtility.GetAssembly("MCI").ToAssemblyDefinition();
                 var engineAssembly = EngineAssemblyDefinition();
-
                 var modAssembly = CreateAssembly();
-                Process(mainAssembly, editorAssembly, engineAssembly);
+
+                Process(mainAssembly, editorAssembly, engineAssembly, modAssembly);
                 //modAssembly.Write("Mod.dll");
             }
             finally
@@ -43,10 +44,14 @@ namespace Mono_Cecil_Sample.Script
             }
         }
 
-        private static void Process(Assembly mainAssembly, AssemblyDefinition editorAssemblyDefinition, AssemblyDefinition engineAssemblyDefinition)
+        private static void Process
+            (
+                AssemblyDefinition mainAssemblyDefinition,
+                AssemblyDefinition editorAssemblyDefinition,
+                AssemblyDefinition engineAssemblyDefinition,
+                AssemblyDefinition modAssemblyDefinition
+            )
         {
-            var mainAssemblyDefinition = mainAssembly.ToAssemblyDefinition(CecilUtility.ReadAndWrite);
-            
             var mainModuleDefinition = mainAssemblyDefinition.MainModule;
             var editorModuleDefinition = editorAssemblyDefinition.MainModule;
             var engineModuleDefinition = engineAssemblyDefinition.MainModule;
@@ -58,11 +63,41 @@ namespace Mono_Cecil_Sample.Script
                               .Types
                               .Where(x => CecilUtility.IsExistAttributeInGlobal(x, modAttributeFullName))
                               .ToArray();
-            
+
+            var classes = new List<TypeDefinition>();
+            var enums = new List<TypeDefinition>();
+            var interfaces = new List<TypeDefinition>();
+
+            Definitions(in definitions, ref classes, ref enums, ref interfaces);
+        }
+
+        private static void Definitions
+        (
+            in TypeDefinition[] definitions,
+            ref List<TypeDefinition> classes,
+            ref List<TypeDefinition> enums,
+            ref List<TypeDefinition> interfaces
+        )
+        {
             foreach (var definition in definitions)
             {
-                Debug.Log(definition.Name);
-                
+                if (definition.IsInterface)
+                {
+                    Debug.Log($"interface {definition.Name}");
+                    
+                    interfaces.Add(definition);
+                    continue;
+                }
+
+                if (definition.IsEnum)
+                {
+                    Debug.Log($"enum {definition.Name}");
+                    enums.Add(definition);
+                    continue;
+                }
+
+                Debug.Log($"class {definition.Name}");
+                classes.Add(definition);
             }
         }
 
